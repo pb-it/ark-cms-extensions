d2d5674d8ab90bfcf5b6944732d9e41771198afc
pb-it <admin@pb-it.at>
2024-02-13 10:09:43 +0100

    :sparkles: Small enhancements

--- src/browser-bookmarks/models/browser-bookmarks.js
+++ src/browser-bookmarks/models/browser-bookmarks.js
@@ -7,15 +7,34 @@ const definition = {
     "attributes": [
         {
             "name": "profile",
-            "dataType": "string"
+            "dataType": "string",
+            "required": true
         },
         {
             "name": "dump",
             "dataType": "json",
             "size": "15"
+        },
+        {
+            "name": "title",
+            "dataType": "string",
+            "persistent": false,
+            "hidden": true
         }
     ],
-    "tableName": "browser_bookmarks"
+    "tableName": "browser_bookmarks",
+    "defaults": {
+        "title": "title"
+    },
+    "extensions": {}
 }
+
+definition['extensions']['client'] = `function init() {
+    this._prepareDataAction = function (data) {
+        data['title'] = data['profile'] + ' - ' + new Date(data['created_at']).toISOString().split('T')[0];
+        return data;
+    }
+}
+export { init };`;
 
 module.exports = definition;
